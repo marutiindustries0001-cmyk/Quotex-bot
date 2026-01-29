4ed958091deb898ac022eefd4047aaabca10a9f8
marutiindustries0001-cmyk <marutiindustries0001@gmail.com>
2026-01-29 19:43:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -155,4 +155,11 @@ if __name__=="__main__":
     threading.Thread(target=start_bot,daemon=True).start()
     threading.Thread(target=daily_report,daemon=True).start()
     port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    socketio.run(
+    app,
+    host="0.0.0.0",
+    port=port,
+    debug=False,
+    allow_unsafe_werkzeug=True
+)
+
